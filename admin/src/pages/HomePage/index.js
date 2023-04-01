57c8e6bd7936a048733657f06a8d9405e2fab8b8
Renaud Heluin <renaud.heluin@novagaia.fr>
2023-04-01 08:59:48 +0200

    Add Helmet title to admin page

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -4,37 +4,28 @@
  *
  */
 
-import { BaseHeaderLayout, ContentLayout } from "@strapi/design-system/Layout";
-import {
-  Box,
-  Checkbox,
-  Divider,
-  EmptyStateLayout,
-  Flex,
-  Icon,
-  Stack,
-  Typography,
-} from "@strapi/design-system";
-import {
-  LoadingIndicatorPage,
-  useAutoReloadOverlayBlocker,
-} from "@strapi/helper-plugin";
-import React, { memo, useEffect, useRef, useState } from "react";
+import { BaseHeaderLayout, ContentLayout } from '@strapi/design-system/Layout';
+import { EmptyStateLayout, Flex } from '@strapi/design-system';
+import { LoadingIndicatorPage, useAutoReloadOverlayBlocker } from '@strapi/helper-plugin';
+import React, { memo, useEffect, useRef, useState } from 'react';
 
-import { Illo } from "../../components/Illo";
-import { InstanceCard } from "../../components/InstanceCard";
-import publisher from "../../api/publisher";
+import { Helmet } from 'react-helmet';
+import { Illo } from '../../components/Illo';
+import { InstanceCard } from '../../components/InstanceCard';
+import pluginPkg from '../../../../package.json';
+import publisher from '../../api/publisher';
+
+const name = pluginPkg.strapi.displayName;
 
 const HomePage = () => {
-  const waitMessage = "loading...";
+  const waitMessage = 'loading...';
   const [welcomeMessage, setWelcomeMessage] = useState(waitMessage);
   const [instances, setInstances] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [shouldEffect, setShouldEffect] = useState(false);
   const publisherComponent = useRef({});
 
-  const { lockAppWithAutoreload, unlockAppWithAutoreload } =
-    useAutoReloadOverlayBlocker();
+  const { lockAppWithAutoreload, unlockAppWithAutoreload } = useAutoReloadOverlayBlocker();
 
   useEffect(() => {
     publisher.getWelcomeMessage().then((res) => {
@@ -73,6 +64,7 @@ const HomePage = () => {
 
   return (
     <>
+      <Helmet title={`${name} configuration`} />
       <BaseHeaderLayout
         title="Nova Publisher"
         subtitle={`Publier vos articles, pages... ${welcomeMessage}`}
@@ -80,14 +72,11 @@ const HomePage = () => {
       />
       <ContentLayout>
         {instances.length === 0 ? (
-          <EmptyStateLayout
-            icon={<Illo />}
-            content="Vous n'avez aucune instance de configurée."
-          />
+          <EmptyStateLayout icon={<Illo />} content="Vous n'avez aucune instance de configurée." />
         ) : (
           <Flex gap={8} direction="column">
             {instances.map((instance) => {
-              if (instance.enabled === "true") {
+              if (instance.enabled === 'true') {
                 return <InstanceCard datas={instance} />;
               }
             })}
